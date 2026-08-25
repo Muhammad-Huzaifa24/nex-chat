0d9c41e75fb8975e58f3da859d8d4d1efc618a0a
Muhammad-Huzaifa24 <huzaifakhawar100@gmail.com>
2026-08-25 14:36:31 +0500

    Update backend deployed URL

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,11 +7,11 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://nex-chat-wjpg.vercel.app',
         changeOrigin: true,
       },
       '/socket.io': {
-        target: 'http://localhost:5000',
+        target: 'https://nex-chat-wjpg.vercel.app',
         ws: true,
       },
     },
